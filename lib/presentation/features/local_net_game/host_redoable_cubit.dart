0fd0af41f120c3e6c8ae9aebb722f02c988b2d84
Faruk Emre <faruk.eksioglu@hotmail.com>
2021-02-19 03:24:01 +0300

    created host redoable cubit

---
+++ lib/presentation/features/local_net_game/host_redoable_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HostRedoableCubit extends Cubit<bool> {
+  HostRedoableCubit() : super(false);
+  
+  void redoable() => emit(true);
+  void nonredoable() => emit(false);
+}
