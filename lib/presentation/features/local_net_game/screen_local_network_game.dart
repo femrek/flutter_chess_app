938d4e558596dbaa60a159cf640895b1d7de2d59
Faruk Emre <faruk.eksioglu@hotmail.com>
2021-02-19 03:10:53 +0300

    added restart function to local host screen

--- lib/presentation/features/local_net_game/screen_local_network_game.dart
+++ lib/presentation/features/local_net_game/screen_local_network_game.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mychess/presentation/features/local_net_game/local_host_event.dart';
 
 import 'host_checkmate_cubit.dart';
+import 'local_host_bloc.dart';
 import 'single_player_chess_table.dart';
 
 class ScreenLocalNetGame extends StatefulWidget {
@@ -27,6 +29,12 @@ class _ScreenLocalNetGameState extends State<ScreenLocalNetGame> {
               return Text(checkmate ? 'checkmate' : '');
             },
           ),
+          RaisedButton(
+            onPressed: () {
+              context.read<LocalHostBloc>().add(LocalHostLoadEvent(restart: true));
+            },
+            child: Text('restart'),
+          )
         ],
       ),
       backgroundColor: Colors.black45,
