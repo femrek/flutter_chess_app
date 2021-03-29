3365dcc900dc2e35e417499f24525074b4349efa
Faruk Emre <faruk.eksioglu@hotmail.com>
2021-03-29 18:21:12 +0300

    removed buildWhen check in single player game

--- lib/presentation/features/local_game/chess_table.dart
+++ lib/presentation/features/local_game/chess_table.dart
@@ -78,38 +78,7 @@ class ChessTable extends StatelessWidget {
           width: size,
         );
       },
-      buildWhen: (oldState, newState) {
-        final String name = '${String.fromCharCode(x+97)}${y+1}';
-        if (newState is BoardLoadedState) {
-          if (oldState is BoardFocusedState) {
-            /*final bool a1 = (name!='a1' && name!='a8' && name!='h1' && name!='h8');
-            final bool a3 = (!oldState.movableCoors.contains(name));
-            final bool a4 = (oldState.focusedCoor != name);
-            final bool a5 = (!newState.movablePiecesCoors.contains(name));
-            final bool a6 = (oldState.lastMoveFrom != name);
-            final bool a7 = (oldState.lastMoveTo != name);
-            print('$name ---- 1: $a1 | 3: $a3 | 4: $a4 | 5: $a5 | 6: $a6 | 7: $a7');
-            if (a1 && a3 && a4 && a5 && a6 && a7) return false;*/
-            if (   (!newState.movablePiecesCoors.contains(name))
-                && (!oldState.movableCoors.contains(name))
-                && (oldState.focusedCoor != name)
-                && (oldState.lastMoveFrom != name)
-                && (oldState.lastMoveTo != name)
-                && (name!='a1' && name!='a8' && name!='h1' && name!='h8'))
-              return false;
-          }
-        } else if (newState is BoardFocusedState) {
-          if (oldState is BoardLoadedState) {
-            if (!newState.movableCoors.contains(name)
-                && newState.focusedCoor != name
-                && !oldState.movablePiecesCoors.contains(name))
-              return false;
-          }
-        }
-        return true;
-      },
     );
-  
   }
 
   Column _letterColumn(BuildContext context, bool isRight) {
