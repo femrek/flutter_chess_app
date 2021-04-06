985753ae850f430cf76fb95937a5b03deedce4d5
Faruk Emre <faruk.eksioglu@hotmail.com>
2021-04-06 17:22:06 +0300

    moved lastMoveEffect to app_theme.dart from utils.dart

--- lib/data/app_theme.dart
+++ lib/data/app_theme.dart
@@ -5,3 +5,4 @@ final Color darkBgColor = Colors.teal;
 final Color lightBgColor = Colors.white;
 final Color blackPiecesColor = Colors.black;
 final Color whitePiecesColor = Colors.orange;
+final Color lastMoveEffect = Colors.blue.withOpacity(0.5);
--- lib/utils.dart
+++ lib/utils.dart
@@ -30,5 +30,3 @@ String getFenFromBundleString(String bundleString) {
 
 const double PI = 3.1415926535;
 const double ninetyDegree = PI / 2;
-
-final Color lastMoveEffect = Colors.blue.withOpacity(0.5);
