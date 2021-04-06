c23411cda3c37a4d16a493913b372c1a3a040966
Faruk Emre <faruk.eksioglu@hotmail.com>
2021-04-06 18:21:15 +0300

    add comments to stoarageManager functions

--- lib/data/storage_manager.dart
+++ lib/data/storage_manager.dart
@@ -16,6 +16,7 @@ class StorageManager {
 
   SharedPreferences _sharedPreferences;
 
+  /// set _sharedPreferences value if it is null. CALL WITH AWAIT
   Future _setSharedPreferences() async {
     if (_sharedPreferences == null) 
       _sharedPreferences = await SharedPreferences.getInstance();
@@ -23,6 +24,8 @@ class StorageManager {
 
   // TWO PLAYER GAME ------------------------------------------------------------------------------
   String _lastGameFen;
+  /// param fen: board state in fen format.
+  /// return: if save success return true else false.
   Future<bool> setLastGameFen(String newFen) async {
     await _setSharedPreferences();
     if (await _sharedPreferences.setString(LAST_GAME_FEN, newFen)) {
@@ -31,6 +34,8 @@ class StorageManager {
     }
     return false;
   }
+  /// return: saved last board state in fen format. (use setLastGameFen for
+  /// save last fen).
   Future<String> get lastGameFen async {
     if (_lastGameFen != null) return _lastGameFen;
     await _setSharedPreferences();
@@ -38,6 +43,7 @@ class StorageManager {
     return _lastGameFen;
   }
   LastMoveModel _lastGameLastMove;
+  /// return: if save success return true else false.
   Future<bool> setLastGameLastMove(LastMoveModel newMove) async {
     await _setSharedPreferences();
     if (await _sharedPreferences.setString(LAST_GAME_LAST_FROM, newMove.from)
@@ -48,6 +54,7 @@ class StorageManager {
     }
     return false;
   }
+  /// return: saved last move. (use setLastGameLastMove for save last move).
   Future<LastMoveModel> get lastGameLastMove async {
     if (_lastGameLastMove != null) return _lastGameLastMove;
     await _setSharedPreferences();
@@ -57,6 +64,7 @@ class StorageManager {
     return _lastGameLastMove;
   }
   List<String> _boardStateHistory;
+  /// return: if save success return true else false.
   Future<bool> setBoardStateHistory(List<String> newList) async {
     await _setSharedPreferences();
     if (await _sharedPreferences.setStringList(GAME_STATE_HISTORY, newList)) {
@@ -65,14 +73,22 @@ class StorageManager {
     }
     return false;
   }
+  /// return: a list of bundle string like "fen#a1/a5". Bundle string is the
+  /// data type for save board state and last move.
   Future<List<String>> get boardStateHistory async {
     if (_boardStateHistory != null) return _boardStateHistory;
     await _setSharedPreferences();
-    _boardStateHistory = _sharedPreferences.getStringList(GAME_STATE_HISTORY) ?? List();
+    _boardStateHistory =
+        _sharedPreferences.getStringList(GAME_STATE_HISTORY) ?? List();
     return _boardStateHistory;
   }
+  /// param bundleString: Bundle string is the data type sent
+  /// from the host to the client. Like "fen#a1/a5".
+  /// return: if save success return true else false.
   Future<bool> addBoardStateHistory(String bundleString) async =>
     await setBoardStateHistory((await boardStateHistory)..add(bundleString));
+  /// return: removed bundle string like "fen#a1/a5". Bundle string is the data
+  /// type for save board state and last move.
   Future<String> removeLastFromBoardStateHistory() async {
     final List<String> currentHistoryList = (await boardStateHistory);
     final String removedState = currentHistoryList.removeLast();
@@ -83,6 +99,8 @@ class StorageManager {
 
   // HOST GAME -------------------------------------------------------------------------------------
   String _lastHostGameFen;
+  /// param fen: board state in fen format.
+  /// return: if save success return true else false.
   Future<bool> setLastHostGameFen(String newFen) async {
     await _setSharedPreferences();
     if (await _sharedPreferences.setString(LAST_HOST_GAME_FEN, newFen)) {
@@ -91,6 +109,8 @@ class StorageManager {
     }
     return false;
   }
+  /// return: saved last board state in fen format. (use setLastHostGameFen for
+  /// save last fen).
   Future<String> get lastHostGameFen async {
     if (_lastHostGameFen != null) return _lastHostGameFen;
     await _setSharedPreferences();
@@ -98,6 +118,7 @@ class StorageManager {
     return _lastHostGameFen;
   }
   LastMoveModel _lastHostGameLastMove;
+  /// return: if save success return true else false.
   Future<bool> setLastHostGameLastMove(LastMoveModel newMove) async {
     await _setSharedPreferences();
     if (await _sharedPreferences.setString(LAST_HOST_GAME_LAST_FROM, newMove.from)
@@ -108,6 +129,7 @@ class StorageManager {
     }
     return false;
   }
+  /// return: saved last move. (use setLastHostGameLastMove for save last move)
   Future<LastMoveModel> get lastHostGameLastMove async {
     if (_lastHostGameLastMove != null) return _lastHostGameLastMove;
     await _setSharedPreferences();
@@ -117,6 +139,7 @@ class StorageManager {
     return _lastHostGameLastMove;
   }
   List<String> _hostBoardStateHistory;
+  /// return: if save success return true else false.
   Future<bool> setHostBoardStateHistory(List<String> newList) async {
     await _setSharedPreferences();
     if (await _sharedPreferences.setStringList(HOST_GAME_STATE_HISTORY, newList)) {
@@ -125,14 +148,21 @@ class StorageManager {
     }
     return false;
   }
+  /// return: a list of bundle string like "fen#a1/a5". Bundle string is the
+  /// data type for save board state and last move.
   Future<List<String>> get hostBoardStateHistory async {
     if (_hostBoardStateHistory != null) return _hostBoardStateHistory;
     await _setSharedPreferences();
     _hostBoardStateHistory = _sharedPreferences.getStringList(HOST_GAME_STATE_HISTORY) ?? List();
     return _hostBoardStateHistory;
   }
+  /// param bundleString: Bundle string is the data type sent
+  /// from the host to the client. Like "fen#a1/a5".
+  /// return: if save success return true else false.
   Future<bool> addHostBoardStateHistory(String bundleString) async =>
     await setHostBoardStateHistory((await hostBoardStateHistory)..add(bundleString));
+  /// return: removed bundle string like "fen#a1/a5". Bundle string is the data
+  /// type for save board state and last move.
   Future<String> removeLastFromHostBoardStateHistory() async {
     final List<String> currentHistoryList = (await hostBoardStateHistory);
     final String removedState = currentHistoryList.removeLast();
@@ -142,6 +172,7 @@ class StorageManager {
 
   // CLIENT ------------------------------------------------------------------------------------
   String _lastConnectedHost;
+  /// return: if save success return true else false.
   Future<bool> setLastConnectedHost(String newHost) async {
     await _setSharedPreferences();
     if (await _sharedPreferences.setString(LAST_CONNECTED_HOST, newHost)) {
@@ -150,6 +181,7 @@ class StorageManager {
     }
     return false;
   }
+  /// return: saved last connected host. (use setLastConnectedHost for save).
   Future<String> get lastConnectedHost async {
     if (_lastConnectedHost != null) return _lastConnectedHost;
     await _setSharedPreferences();
@@ -158,6 +190,7 @@ class StorageManager {
   }
 
   int _lastConnectedPort;
+  /// return: if save success return true else false.
   Future<bool> setLastConnectedPort(int newPort) async {
     await _setSharedPreferences();
     if (await _sharedPreferences.setInt(LAST_CONNECTED_PORT, newPort)) {
@@ -166,6 +199,7 @@ class StorageManager {
     }
     return false;
   }
+  /// return: saved last connected port. (use setLastConnectedPort for save).
   Future<int> get lastConnectedPort async {
     if (_lastConnectedPort != null) return _lastConnectedPort;
     await _setSharedPreferences();
@@ -173,6 +207,7 @@ class StorageManager {
     return _lastConnectedPort;
   }
 
+  // singleton-----------------------------------------------------------------------------------
   static final StorageManager _instance = StorageManager._internal();
   StorageManager._internal();
   factory StorageManager() =>_instance; 
