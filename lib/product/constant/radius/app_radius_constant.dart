7760f1061866fbaef7d90a2902010f12da68b216
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2024-08-27 01:17:52 +0300

    style: pseudo raduis values remvoed.

--- lib/product/constant/radius/app_radius_constant.dart
+++ lib/product/constant/radius/app_radius_constant.dart
@@ -1,8 +1,2 @@
 /// This file contains the radius constants used in the app.
-abstract final class AppRadiusConstant {
-  /// Default radius for the buttons
-  static const double button = 6;
-
-  /// Default radius for the card
-  static const double card = 12;
-}
+abstract final class AppRadiusConstant {}
