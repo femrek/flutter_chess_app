44fb5a8825ef3654b4fe8d07d96d693a2f13c876
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2024-09-02 04:21:59 +0300

    doc: missing doc for local_game_header.dart

--- lib/feature/local_game/view/widget/local_game_header.dart
+++ lib/feature/local_game/view/widget/local_game_header.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+/// The header of the local game screen.
 class LocalGameHeader extends StatelessWidget implements PreferredSizeWidget {
+  /// Creates the local game header.
   const LocalGameHeader({
     required this.gameName,
     super.key,
   });
 
+  /// The name of the game to be displayed.
   final String gameName;
 
   @override
