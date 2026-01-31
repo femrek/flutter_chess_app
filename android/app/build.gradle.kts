fe12e07eaaf29d84d757276dd808c19b0086dc06
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2026-01-31 21:57:22 +0300

    feat: update Android project configuration to new structure. update gradle as well.

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,59 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "dev.faruke.chess.localchess"
+    compileSdk = 36
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "dev.faruke.chess.localchess"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = (keystoreProperties["storeFile"] as String?)?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
--- android/app/src/main/kotlin/dev/faruke/chess/localchess/MainActivity.kt
+++ android/app/src/main/kotlin/dev/faruke/chess/localchess/MainActivity.kt
@@ -1,11 +1,5 @@
 package dev.faruke.chess.localchess
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
-import java.net.InetAddress
-import java.net.NetworkInterface
-import java.net.SocketException
-import java.util.*
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
